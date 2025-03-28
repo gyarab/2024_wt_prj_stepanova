26a07fdb649736779f5a483f8a3410c545fff4b3
KiraStepanova <148944110+KiraStepanova@users.noreply.github.com>
2025-03-28 10:16:02 +0100

    databaze start

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe
+from .models import Cuisine
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "difficulty", "calories"]
+
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
 
 # Register your models here.
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Cuisine, CuisineAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=300)
+    difficulty = models.IntegerField(blank=True, null=True)
+    calories = models.IntegerField(blank=True, null=True)
+    nutriention table =  
+    cuisine = models.ForeignKey("Cuisine", on_delete=models.SET_NULL, null=True)
+    
+
+
+    def __str__(self):
+        return f"Recipe <{self.name}> {self.year}"
+        return f"Cuisine <{self.name}>"
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=300)
